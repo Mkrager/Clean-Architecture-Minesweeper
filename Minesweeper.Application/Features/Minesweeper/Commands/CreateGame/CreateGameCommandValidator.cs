1a1c2d4b9fa22a35682258c092b42df2747a8fb5
Smaga Max <smaga.max@gmail.com>
2025-07-30 00:43:38 +0300

    add validation behaviour

---
+++ Minesweeper.Application/Features/Minesweeper/Commands/CreateGame/CreateGameCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Minesweeper.Application.Features.Minesweeper.Commands.CreateGame
+{
+    public class CreateGameCommandValidator : AbstractValidator<CreateGameCommand>
+    {
+        public CreateGameCommandValidator()
+        {
+            
+        }
+    }
+}
